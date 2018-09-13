83a571ca19f76b66b599b4179609e4ba92551821
TALITHA <TALITHA@LAPTOP-RTUP5796>
2018-09-13 13:05:29 +0700

    Tambah Tampil Hasil

--- src/hitungfibonacci/HitungFibonacci.java
+++ src/hitungfibonacci/HitungFibonacci.java
@@ -16,6 +16,7 @@ public class HitungFibonacci {
         
         int n = tampilInput();
         BigInteger hasil = fibo(n);
+        tampilHasil(n, hasil);
     }
     public static int tampilInput() {
         Scanner scanner = new Scanner(System.in);
@@ -36,6 +37,10 @@ public class HitungFibonacci {
             hasil[i] = hasil[i-1].add(hasil[i-2]);
         }
         return hasil[n-1];
+    }
+    private static void tampilHasil(int n, BigInteger hasil)
+    {
+        System.out.println("Bilangan Fibonacci ke-"+n+ " : " + hasil);
     }
     
 }
