b3c227b496b737e9c53bca96f59065218669a687
TALITHA <TALITHA@LAPTOP-RTUP5796>
2018-09-14 22:05:15 +0700

    Initial commit

---
+++ src/J07/Tugas02.java
@@ -0,0 +1,11 @@
+package J07;
+import java.util.Scanner;
+public class Tugas02 {
+    public static void main(String[] args)
+    {
+        String identitas = "Talitha Sevrilla Duriga / XRPL2 / 35";
+        System.out.println("Identitas: " + identitas);
+        System.out.println("");
+    }
+    
+}
