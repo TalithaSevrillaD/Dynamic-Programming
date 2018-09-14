5527ae6f6b74f801bce3014c79891edd7a14590a
TALITHA <TALITHA@LAPTOP-RTUP5796>
2018-09-14 22:14:03 +0700

    Tambah Proses dan hasil

--- src/J07/Tugas02.java
+++ src/J07/Tugas02.java
@@ -5,6 +5,7 @@ public class Tugas02 {
     {
         int [] kilogram = new int[] {7, 5, 4, 3, 2};
         int nominal[] = { 10000, 7000, 5000,3000, 2000};
+        char [] jenis = new char [] {'E', 'B','A','D','C'};
         
         String identitas = "Talitha Sevrilla Duriga / XRPL2 / 35";
         System.out.println("Identitas: " + identitas);
@@ -15,6 +16,16 @@ public class Tugas02 {
         int berat = baca.nextInt();
         int jumlah = 0, i, bayar = 0;
         
+        for(i = 0; i < 5; i++)
+        {
+            jumlah = berat/kilogram[i];
+            berat = berat - (kilogram[i]*jumlah);
+            bayar += jumlah*nominal[i];
+            char barang = jenis[i];
+            
+            System.out.println("Jenis barang" + barang + "berat" + kilogram[i] + "kg" + ":" + jumlah);
+        }
+        System.out.println("Bayaran yang didapat : " + bayar);
     }
     
 }
