3f6f71e35c201f9004a6fade0ef2427f1723abca
TALITHA <TALITHA@LAPTOP-RTUP5796>
2018-09-14 21:57:42 +0700

    Tambah Input

--- src/J07/Tugas01.java
+++ src/J07/Tugas01.java
@@ -3,9 +3,16 @@ import java.util.Scanner;
 public class Tugas01 {
     public static void main(String[] args)
     {
+        int [] nominal = new int[] {5000,2000, 1000, 500, 100};
+        int uang, i,jumlah, n = nominal.length;
+        
         String identitas = "Talitha Sevrilla Duriga / XRPL2 / 35";
         System.out.println("Identitas: " + identitas);
         System.out.println("");
+        
+        Scanner baca = new Scanner(System.in);
+        System.out.print("Masukkan Jumlah uang: ");
+        uang = baca.nextInt();
     }
     
 }
