cfc1e9ab401c2f1807f65d434fd60540e6e5a045
TALITHA <TALITHA@LAPTOP-RTUP5796>
2018-09-14 22:08:51 +0700

    Tambah Input

--- src/J07/Tugas02.java
+++ src/J07/Tugas02.java
@@ -3,9 +3,18 @@ import java.util.Scanner;
 public class Tugas02 {
     public static void main(String[] args)
     {
+        int [] kilogram = new int[] {7, 5, 4, 3, 2};
+        int nominal[] = { 10000, 7000, 5000,3000, 2000};
+        
         String identitas = "Talitha Sevrilla Duriga / XRPL2 / 35";
         System.out.println("Identitas: " + identitas);
         System.out.println("");
+        
+        Scanner baca = new Scanner (System.in);
+        System.out.print("Masukkan max berat barang: ");
+        int berat = baca.nextInt();
+        int jumlah = 0, i, bayar = 0;
+        
     }
     
 }
